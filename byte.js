8a41c4058c04b84cb174b3b14812a43abb5c031e
soumo1414 <soumosantra588@gmail.com>
2026-01-16 14:19:42 +0530

    init: soumo

---
+++ byte.js
@@ -0,0 +1,24 @@
+let a=6 //OUTPUT 110
+let b=7 //OUTPUT 111
+console.log("Logical And Operator=",a&b)
+// ouput 6 110
+
+let d=8 //OUTPUT 1000
+let e=12 //OUTPUT 1100
+console.log("Logical Or Operator=",d|e)
+// output 12 1100
+
+let f=12 //OUTPUT 1100
+console.log("Logical Not Operator=",(~f))
+//output -13
+
+let g=-22 //output -10110
+console.log("Logical Not Operator=",(~g))
+//OUTPUT 21 10101
+
+let h=15 //OUTPUT 1111
+let i=20 //OUTPUT 10100
+console.log("Logical Xor Operator=",h^i)
+//
+
+ 
