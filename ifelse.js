d3a4b8292540a46301eb7872f308ba50dd164e26
soumo1414 <soumosantra588@gmail.com>
2026-01-20 13:51:28 +0530

    init: soumo

---
+++ ifelse.js
@@ -0,0 +1,28 @@
+let a=18
+if (a>=21){
+    console.log("you eligible for marriage")
+}else{
+    console.log("you not eligible for marriage")
+}
+
+
+let b=26
+if (b>=21){
+    console.log("you eligible for marriage")
+}else{
+    console.log("you not eligible for marriage")
+}
+
+let c=23
+if (c>=18){
+    console.log("The girl is now old enough to get married")
+}else{
+    console.log("The girl is not yet old enough to get married")
+}
+
+let d=17
+if (d>=18){
+    console.log("The girl is now old enough to get married")
+}else{
+    console.log("The girl is not yet old enough to get married")
+}
---
+++ sequeential.js
@@ -0,0 +1,4 @@
+let x=98
+let y=56
+let z=x+y
+console.log("This is Sequential Flow =",z)
