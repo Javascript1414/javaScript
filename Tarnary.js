ff2e15e735471dd9b8fd26e234b09c94628daf44
soumo1414 <soumosantra588@gmail.com>
2026-01-20 16:15:12 +0530

    init: soumo

---
+++ Tarnary.js
@@ -0,0 +1,7 @@
+let a=5
+let b=10
+console.log(a>b ?"a is grater than b":a<b ?"b is grater than a":"a is equal to b")
+
+let age=19
+console.log(age>=18 ?"Are you adult person":age<18 ?"You are a teenager":"You are a kid")
+
