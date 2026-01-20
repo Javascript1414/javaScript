0bbd8c9f67fab9f4efdf9368775af75fdd063889
soumo1414 <soumosantra588@gmail.com>
2026-01-20 15:01:56 +0530

    init:spumo

---
+++ nestedif.js
@@ -0,0 +1,14 @@
+let age=18;
+let pancard=true;
+if (age>=18){
+    console.log("Adult Person");
+    if (pancard){  
+        console.log("Pan card ka liya apply kar sakta hai");
+}
+    else{
+        console.log("Adult Hai par pan card nahi apply kar sakta");
+    }
+}
+else{
+    console.log("Ab abhi bacca ho vaii");
+    }
