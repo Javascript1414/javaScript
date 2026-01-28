5a7a491d7cf89c3395d41cab1db105ea2cc7e415
soumo1414 <soumosantra588@gmail.com>
2026-01-28 17:06:06 +0530

    init: soum

--- ternarycalculator.js
+++ ternarycalculator.js
@@ -12,5 +12,22 @@ function divide(a, b) {
     return a/b
 }
 
-function cal(a,b,ope)
-{}
+function cal(a,b,ope){
+
+    return (ope=="+")?add(a,b):
+    (ope=="-")?subtract(a,b):
+    (ope=="*")?multiply(a,b):
+    (ope=="/")?divide(a,b):
+    "invalid operator"
+
+}
+
+ console.log("Tearnary operatior result:")
+ console.log("Addition=",cal(20,5,"+")); 
+ console.log("Subtraction=",cal(20,10,"-"))
+
+
+
+
+ 
+console.log(12,10,"//")
