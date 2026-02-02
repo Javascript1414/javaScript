4b8a0e97ffbddc39d2d50447dc2c98c78bb5de05
soumo1414 <soumosantra588@gmail.com>
2026-02-02 14:45:35 +0530

    init: so

---
+++ prime.js
@@ -0,0 +1,13 @@
+function prime(e) {
+    let g;
+    for (g=4; g<e; g++){
+        if(e%g===0){
+            return ("Number is not prime");
+            break;
+        }else{
+            return ("Number is prime");
+            break;
+        }
+    }
+}
+console.log("It is",prime(8))
