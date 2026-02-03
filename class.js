672838d9cbc72442efa1b042aea26f40ee733e37
javascript1414 <soumosantra588@gmail.com>
2026-02-03 15:03:58 +0530

    init: sou

---
+++ class.js
@@ -0,0 +1,18 @@
+class Address{
+    constructor(V1,P2,P3,D4){
+        this.VILL=V1;
+        this.POST=P2;
+        this.PS=P3;
+        this.DIST=D4;
+    }
+}
+
+const Address1= new Address("Bahirtafa","Jadurberia","Uluberia","Howrah");
+console.log(Address1.VILL)
+console.log(Address1.POST)
+console.log(Address1.PS)
+console.log(Address1.DIST)
+
+
+
+
---
+++ object.js
@@ -0,0 +1,13 @@
+const person ={
+    Name: "Soumyajit",
+    Roll: 12,
+    Address: "Bahirtafa",
+    Post_office: "Jadurberia",
+
+
+}
+
+console.log(person)
+console.log(person.Roll)
+console.log(person.Post_office)
+
