3201abcd850455d57737cba40031960096e60120
javascript1414 <soumosantra588@gmail.com>
2026-02-10 09:43:56 +0530

    init: s

--- inheritance1.js
+++ inheritance1.js
@@ -1,11 +1,40 @@
 class Details{
-    constructor (a
-        
-        
-        
-        
-        
-        me,Father_name,Mother_Name)
+    constructor (fname,mname,lname){
 
-
+        this.Full_name=fname
+        this.middle_Name=mname
+        this.Last_name=lname
+    }
+        
 }
+
+class Aadhaar extends Details{
+    constructor (fname,mname,lname,Aadhaar, Pan){
+    super(fname,mname,lname)
+    this.Aadhaar=Aadhaar
+    this.Pan=Pan
+}
+}
+
+class Email extends Aadhaar{
+    constructor (fname,mname,lname,Aadhaar, Pan, Email){
+        super (fname,mname,lname,Aadhaar, Pan)
+        this.Email=Email
+    }
+}
+   
+let Soumo=new Email("Soumo","Chandra","Das",5896523695,"ISPTC8523E","soumo@gmail.com");
+console.log(Soumo)
+console.log(Soumo.Full_name)
+console.log(Soumo.middle_Name)
+console.log(Soumo.Last_name)
+console.log(Soumo.Aadhaar)
+console.log(Soumo.Pan)
+console.log(Soumo.Email)
+
+
+        
+        
+    
+
+
