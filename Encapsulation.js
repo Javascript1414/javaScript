4bda0a1383a5543de932064b5bbf8eddf7adfbad
javascript1414 <soumosantra588@gmail.com>
2026-02-10 14:38:09 +0530

    init: s

--- Encapsulation.js
+++ Encapsulation.js
@@ -1,23 +1,4 @@
-class soumo{
-    #Name //private varible decleration
-    constructor(Name,Address,Email){
-        this.#Name=Name
-        this.Address=Address
-        this.Email=Email
-    }
-    get(){
-        return this.Name=this.#Name
-    }
-    set(s){
-        this.#Name=s
-    }
-}
 
-const s=new soumo("Soumo","Bahirtafa","soumo64@gmail.com")
-console.log(s)
-console.log(s.Address)
-console.log(s.Email)
-console.log(s.Name)
-console.log(s.get())
-s.set("Sribas")
-console.log(s.get())
+
+
+
