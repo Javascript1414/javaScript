a1a37b34a049dcbed63e240353d750022336a483
javascript1414 <soumosantra588@gmail.com>
2026-02-24 16:08:22 +0530

    init: s

---
+++ errorhandling2.js
@@ -0,0 +1,13 @@
+function soumo(){
+    let a="Welcome to NSTi Howrah";
+    let b=" Dasnagar"
+     c=a+b
+}
+console.log(soumo())
+try {
+    console.log(c)
+}
+catch (Error){
+    console.log("error")
+}
+console.log("Hare Krishna")
